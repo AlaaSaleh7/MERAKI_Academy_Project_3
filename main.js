609acd1ec04975d32e1aa919075f2ee776042fcc
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-05-13 23:05:58 +0300

    getAllArticles

--- main.js
+++ main.js
@@ -4,3 +4,41 @@ const app = express();
 
 const port = 5000;
 
+const articles = [
+    {
+    id: 1,
+    title: 'How I learn coding?',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    {
+    id: 2,
+    title: 'Coding Best Practices',
+    description:
+    'Lorem, ipsum dolor sit, Quam, mollitia.',
+    author: 'Besslan',
+    },
+    {
+    id: 3,
+    title: 'Debugging',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    ];
+
+
+    app.get('/articles',(req,res)=>{
+        res.status(200);
+        res.json(articles);
+    });
+
+
+
+
+
+
+    app.listen(port,()=>{
+        console.log(`Example app listening at http://localhost:${port}`);
+    })
