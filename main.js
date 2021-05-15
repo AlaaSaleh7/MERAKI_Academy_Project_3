b6d753056c73519eeb45835d24b5cc14a2c9bc8a
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-05-15 12:19:28 +0300

    updateAnArticleById

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ const {uuid} = require("uuidv4");
 
 const app = express();
 
+// number the port 
 const port = 5000;
 
-app.use(express.json())
+// a middleware that enables us to read the received JSON data
+app.use(express.json());
 
+// modify array
 const articles = [
   {
     id: 1,
@@ -29,40 +32,86 @@ const articles = [
   },
 ];
 
+// to get all the articles
+// a Get request on endpoint http://localhost:5000/articles
 app.get("/articles", (req, res) => {
+  // set the response status code to 200 (ok)
   res.status(200);
+  // sends back a response all articles
   res.json(articles);
 });
 
+// to get articles by author
+// a Get request on endpoint http://localhost:5000/articles/search_1
 app.get("/articles/search_1", (req, res) => {
+   // query parameters way: req.query.id "/articles/search_1?author=Jouza"
   let author = req.query.author;
+
   const articlesAuthor = articles.filter(
     (element) => element.author === author
   );
+  // set the response status code to 200 (ok)
   res.status(200);
+  // sends back a response article by author
   res.json(articlesAuthor);
 });
 
+// to get articles by id
+// a Get request on endpoint http://localhost:5000/articles/:id
 app.get("/articles/:id", (req, res) => {
+  // query parameters way: req.query.id "/articles?id=3"
   let id = req.query.id;
-  const articlesId = articles.filter((element) => element.id === parseInt(id));
+
+  const articlesId = articles.filter((element) => element.id === parseInt(id)); 
+  // set the response status code to 200 (ok)
   res.status(200);
+  // sends back a response article by req id
   res.json(articlesId);
 });
 
+// to add new article
+// a Post request on endpoint http://localhost:5000/articles
 app.post("/articles", (req, res) => {
-  let artical = {
+  let article = {
     id: uuid(),
     title: req.body.title,
     description: req.body.description,
     author: req.body.author,
   };
   
-  articles.push(artical);
+  articles.push(article);
+  // set the response status code to 201 (Created)
   res.status(201);
-  res.json(artical);
+  // sends back a response article
+  res.json(article);
 });
 
+
+// to update the article by id 
+// a Put request on endpoint http://localhost:5000/articles/:id
+app.put("/articles/:id",((req, res) => {
+// received route parameters are in req.params
+  const id = req.params.id
+   let i; 
+
+  const found = articles.find((element,index) => {
+      i = index;
+    return element.id === parseInt(id);
+  });
+
+  if (found){
+      articles[i].title = req.body.title
+      articles[i].description=req.body.description
+      articles[i].author=req.body.author
+    // set the response status code to 200 (ok)
+    res.status(200);
+    // sends back a response articles after update
+    res.json(articles);
+  }
+
+}))
+
+// listening app in number of port
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
