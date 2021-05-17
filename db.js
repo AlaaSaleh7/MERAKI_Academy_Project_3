531c4c17da896836a109cd242257f22b5e583858
AlaaSaleh7 <allaasaleeh7@gmail.com>
2021-05-17 14:02:52 +0300

    createNewAuthor

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 
-const options ={
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 };
 
-mongoose.connect('mongodb://localhost:27017/DB_project_3',options).then(
-    ()=>{
-        console.log("DB Ready To Use");
-},(err)=>{
-    console.log(err)
-});
+mongoose.connect("mongodb://localhost:27017/DB_project_3", options).then(
+  () => {
+    console.log("DB Ready To Use");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const express = require("express");
 
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
 
-const theArticles = require("./schema")
+const {Users,Articles} = require("./schema");
 
 const db = require("./db");
 
-const {uuid} = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
-// number the port 
+// number the port
 const port = 5000;
 
 // a middleware that enables us to read the received JSON data
@@ -50,7 +50,7 @@ app.get("/articles", (req, res) => {
 // to get articles by author
 // a Get request on endpoint http://localhost:5000/articles/search_1
 app.get("/articles/search_1", (req, res) => {
-   // query parameters way: req.query.id "/articles/search_1?author=Jouza"
+  // query parameters way: req.query.id "/articles/search_1?author=Jouza"
   let author = req.query.author;
 
   const articlesAuthor = articles.filter(
@@ -68,7 +68,7 @@ app.get("/articles/:id", (req, res) => {
   // query parameters way: req.query.id "/articles?id=3"
   let id = req.query.id;
 
-  const articlesId = articles.filter((element) => element.id === parseInt(id)); 
+  const articlesId = articles.filter((element) => element.id === parseInt(id));
   // set the response status code to 200 (ok)
   res.status(200);
   // sends back a response article by req id
@@ -84,7 +84,7 @@ app.post("/articles", (req, res) => {
     description: req.body.description,
     author: req.body.author,
   };
-  
+
   articles.push(article);
   // set the response status code to 201 (Created)
   res.status(201);
@@ -92,74 +92,91 @@ app.post("/articles", (req, res) => {
   res.json(article);
 });
 
-
-// to update the article by id 
+// to update the article by id
 // a Put request on endpoint http://localhost:5000/articles/:id
-app.put("/articles/:id",(req, res) => {
+app.put("/articles/:id", (req, res) => {
   // received route parameters are in req.params
-  const id = req.params.id
-   let i; 
+  const id = req.params.id;
+  let i;
 
-  const found = articles.find((element,index) => {
-      i = index;
+  const found = articles.find((element, index) => {
+    i = index;
     return element.id === parseInt(id);
   });
-// const {title,description,author}=req.body
-  if (found){
-      articles[i].title = req.body.title
-      articles[i].description=req.body.description
-      articles[i].author=req.body.author
+  // const {title,description,author}=req.body
+  if (found) {
+    articles[i].title = req.body.title;
+    articles[i].description = req.body.description;
+    articles[i].author = req.body.author;
     // set the response status code to 200 (ok)
     res.status(200);
     // sends back a response articles after update
     res.json(articles);
   }
-})
+});
 
 // Delete article by id and return massage Success Delete with id
 // a Delete request on endpoint http://localhost:5000/articles/:id
-app.delete('/articles/:id',(req,res)=>{
+app.delete("/articles/:id", (req, res) => {
   // received route parameters are in req.params
-  const id = req.params.id
-  let i; 
+  const id = req.params.id;
+  let i;
 
-  const found = articles.find((element,index) => {
-      i = index;
+  const found = articles.find((element, index) => {
+    i = index;
     return element.id === parseInt(id);
   });
-  if(found){
-    articles.splice(i,1)
-  };
-  let massage ={"success":true,'Success Delete article with =>': `${id}`}
-   // sends back a response massage after delete article by id
+  if (found) {
+    articles.splice(i, 1);
+  }
+  let massage = { success: true, "Success Delete article with =>": `${id}` };
+  // sends back a response massage after delete article by id
   res.json(massage);
-})
+});
 
 // Delete article by author and return massage Success Delete with author
 // a Delete request on endpoint http://localhost:5000/articles
-app.delete('/articles',(req,res)=>{
+app.delete("/articles", (req, res) => {
+  const author = req.body.author;
+  let delet = {};
 
-const author = req.body.author
-  let delet= {}
-        
-        for (i=0;i<articles.length;i++){
-            if(author===articles[i].author){
-                delet=articles.splice(i,1);
-            }
-        } 
- 
-let massage ={"success":true,'Success Delete article with =>': `${author}`}
+  for (i = 0; i < articles.length; i++) {
+    if (author === articles[i].author) {
+      delet = articles.splice(i, 1);
+    }
+  }
 
-// sends back a response massage after delete article by author
-res.json(massage);
+  let massage = {
+    success: true,
+    "Success Delete article with =>": `${author}`,
+  };
 
+  // sends back a response massage after delete article by author
+  res.json(massage);
 });
 
+//create new author
+// a Post request on endpoint http://localhost:5000/users
+app.post("/users", (req, res) => {
+  const { firstName, lastName, age, country, email, password } = req.body;
 
-//create new author 
-app.post("/users",(req,res)=>{
-  const {firstName,lastName,age, isCompleted, priority } = req.body;
+  const newAuthor = new Users({
+    firstName,
+    lastName,
+    age,
+    country,
+    email,
+    password,
+  });
 
+  newAuthor
+  .save()
+  .then((result)=>{
+    res.json(result)
+  })
+  .catch((err)=>{
+    res.send(err);
+  })
 });
 
 // listening app in number of port
